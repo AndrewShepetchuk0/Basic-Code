830dbd6bde2ee850d3a48323ae55b24a275e9063
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2022-07-17 23:28:09 +0000

    Update password-Generator.py

--- password-Generator.py
+++ password-Generator.py
@@ -1,10 +1,18 @@
 import random
 import string
+#Determines how long the password will be
+
 pl = int(input("How long do you want your password to be?"))
 pw = []
+
+#Creates a list of characters for the password generator to choose from
 char = list("qwertyuiopasdfghjklzxcvbnm" + "QWERTYUIOPASDFGHJKLZXCVBNM" + "1234567890" + "`-=[]\;',./" + "~!@#$%&*()_+{}|:<>?")
+
+#Generates the password
 for i in range(pl):
     random.shuffle(char)
     pw.append(random.choice(char))
 random.shuffle(pw)
+
+#Prints the password
 print("".join(pw))
